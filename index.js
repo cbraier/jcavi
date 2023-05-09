2f60cb5b38e4b3122834efb062009c14048e2da1
Cristiano Braier <cristianobraier@MacBook-Air-de-Cristiano.local>
2023-05-08 22:24:52 -0300

    aula06_exercicio

--- index.js
+++ index.js
@@ -1,104 +1,110 @@
-//string
-//number
-//array
-//boolean
+//String
+//Numeber
+//Array
+//Boolean
 
-//object
+//Object
 
-//let carro = {
-// marca: 'ford',
-// cor: 'branco',
-//ano: 2020,
 
-//}
-
-//carro.modelo = "gol"
-
-//alert(carro.cor)
+// let carro = {
+// marca: 'vw',
+// ano: 2020,
+// cor: 'vermelho'
+// }
+// carro.ano = 2019
+// alert(carro)
 
 
 let carros = [];
 
 //Validar se placa tem 8 "mev-1234"
-//Validar se carros apartir de "2018"
-
-
-
+//Validar se carros apartir de 2018
 function cadastrarCarro() {
+let placa = prompt('Digite a placa')
+let cor = prompt('Digite a cor')
+let ano = prompt('Digite a ano')
 
-    let placa = prompt('digite a placa')
-    let cor = prompt('digite a cor')
-    let ano = prompt('digite o ano')
-
-    let carro = {
-        placa: placa,
-        cor: cor,
-        ano: ano,  //JS ja entende...nao precisa coloca placa: placa....apenas placa,...
-    }
-
-    carros.push(carro)
-
-    executarMenu()
+debugger
+let carro = {
+placa,
+cor: cor,
+ano: ano,
 }
 
+let msgErro = validarCarro(carro)
+if(msgErro == '') {
+carros.push(carro)
+} else {
+alert(msgErro)
+}
+
+executarMenu()
+}
+
+function validarCarro(carro) {
+debugger
+let msg = ''
+if(carro.placa.length != 8) {
+msg += 'Placa deve ter 8 dígitos'
+}
+return msg
+}
+
+//
 function pesquisarCarro() {
-    let placaPesquisa = prompt('Digite a placa de pesquisa!')
+let placaPesquisa = prompt('Digite a placa de pesquisa!')
 
-    for (let i = 0; i < carros.length; i++) {
-        let carro = carros[i]
-
-        if (placaPesquisa == carro.placa) {
-            alert(`
+for (let i = 0; i < carros.length; i++) {
+let carro = carros[i]
+if(placaPesquisa == carro.placa) {
+alert(`
 Placa: ${carro.placa}
 Cor: ${carro.cor}
 Ano: ${carro.ano}
 `)
-            executarMenu()
-        }
-    }
-
-
-    alert('Carro não encontrado')
-    executarMenu()
+executarMenu()
+}
+}
+alert('Carro não encontrado!')
+executarMenu()
 }
 
 function pesquisarCarrosAno() {
-    let ano = prompt('Digite o ano de pesquisa!')
-    let msg = ''
+let ano = prompt('Digite o ano de pesquisa!')
+let msg = ''
 
-    for (let i = 0; i < carros.length; i++) {
-        const carro = carros[i];
-        if (carro.ano == ano) {
-            msg += `Placa: ${carro.placa | Cor: ${ carro.cor } | Ano: $(carro.ano)
-    }
-        }
+for (let i = 0; i < carros.length; i++) {
+const carro = carros[i];
+if(carro.ano == ano) {
+msg += `Placa: ${carro.placa}/Cor: ${carro.cor}/Ano: ${carro.ano} \r\n`
+}
+}
+
 alert(msg)
 executarMenu()
-    }
-
-let carrosEncontrados =
-
-    executarMenu()
-
-//pesquisarCarrosAno
+}
 
 function executarMenu() {
-    let acao = prompt(`
-        (1) Cadastrar automóvel
-        (2) Pesquisar automóvel
-        (3) Pesquisar carros por ano
-        (4) Excluir
-        (5) Atualizar 
-        (6) Sair
-    `)
+let acao = prompt(`
+(1) Cadastrar automóvel
+(2) Pesquisar automóvel
+(3) Pesquisar carros por ano
+(4) Excluir
+(5) Atualizar
+(6) Sair
+`)
 
-    if (acao == "1") {
-        cadastrarCarro()
-    } else if (acao == "2") {
-        pesquisarCarro()
-    }
+if(acao == "1") {
+cadastrarCarro()
+} else if(acao == "2") {
+pesquisarCarro()
+} else if(acao == "3") {
+pesquisarCarrosAno()
+}
 }
 
 executarMenu()
 
 
+
+//[{placa:2020}, {placa:2022}]
